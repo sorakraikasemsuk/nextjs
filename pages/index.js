0e1a5dbc400b3d409960720ad7261f6760621b4a
sorakraikasemsuk <peakung03@gmail.com>
2017-09-15 17:08:51 +0700

    test styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout'
+import styled from 'styled-components';
 
+const Title=styled.h1`
+    color:red;
+`;
+const Button=styled.button`
+    background:white;
+    border:solid 1px black;
+    padding:10px;
+    &:hover{
+        background:red;
+    }
+`;
 class componentName extends Component {
    constructor(props){
        super(props);
@@ -22,6 +34,8 @@ class componentName extends Component {
         return (
             <div>
                <Layout title={'Index'} />
+               <Title className="text-danger">Title</Title>
+               <Button>Test</Button>
                <div className="row">
                    <div className="col-md-12">
                        Hello React With Next.js
